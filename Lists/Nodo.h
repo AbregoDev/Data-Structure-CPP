b746e2315a45bbcf4b5c9c147f738ba3bef157af
abregoDev <abre.go@outlook.com>
2021-03-28 18:18:58 -0600

    Búsqueda lista y clase Nodo

---
+++ Lists/Nodo.h
@@ -0,0 +1,50 @@
+#include <iostream>
+
+typedef int Tipo;
+
+class Nodo
+{
+   public:
+      int dato;
+      Nodo *siguiente;
+
+   public:
+      Nodo();
+      Nodo(int dato);
+      int getDato();
+      void setDato(int dato);
+      Nodo *getSiguiente();
+      void setSiguiente(Nodo *siguiente);
+};
+
+Nodo::Nodo()
+{
+   Nodo::dato = 0;
+   Nodo::siguiente = NULL;
+}
+
+Nodo::Nodo(int dato)
+{
+   Nodo::dato = dato;
+   Nodo::siguiente = NULL;
+}
+
+int Nodo::getDato()
+{
+   return dato;
+}
+
+void Nodo::setDato(int dato)
+{
+   Nodo::dato = dato;
+}
+
+Nodo *Nodo::getSiguiente()
+{
+   return Nodo::siguiente;
+}
+
+void Nodo::setSiguiente(Nodo *siguiente)
+{
+   Nodo::siguiente = siguiente;
+}
--- Lists/list.cpp
+++ Lists/list.cpp
@@ -1,38 +1,6 @@
 #include <iostream>
+#include "Nodo.h"
 using namespace std;
-
-class Nodo
-{
-   int dato;
-   Nodo *siguiente;
-
-   public: Nodo() { }
-
-   public: Nodo(int dato)
-   {
-      Nodo::dato = dato;
-   }
-
-   public: int getDato()
-   {
-      return dato;
-   }
-
-   public: void setDato(int dato)
-   {
-      Nodo::dato = dato;
-   }
-
-   public: Nodo *getSiguiente()
-   {
-      return siguiente;
-   }
-
-   public: void setSiguiente(Nodo *siguiente)
-   {
-      Nodo::siguiente = siguiente;
-   }
-};
 
 class Lista
 {
@@ -43,16 +11,88 @@ class Lista
       cabeza = NULL;
    }
 
-   public: void insertarAlPrincipio(int dato)
+   public: void insertarAlPrincipio(Tipo dato)
    {
       Nodo *nuevoNodo = new Nodo(dato);
       nuevoNodo->setSiguiente(cabeza);
       cabeza = nuevoNodo;
    }
 
-   public: Nodo *getCabeza()
+   public: void insertarAlFinal(Tipo dato)
    {
-      return cabeza;
+      Nodo *nuevoNodo = new Nodo(dato);
+      Nodo *aux = cabeza;
+
+      while(aux != NULL)
+      {
+         if(aux->getSiguiente() == NULL)
+         {
+            // Llegué al último nodo
+            aux->setSiguiente(nuevoNodo);
+            break;
+         }
+         else
+         {
+            aux = aux->getSiguiente();
+         }
+      }
+   }
+
+   public: int buscarElemento(Tipo elementoBuscado)
+   {
+      Nodo *aux = cabeza;
+      int indice = 0;
+      while(aux != NULL)
+      {
+         if(aux->getDato() == elementoBuscado)
+         {
+            break;
+         }
+         else
+         {
+            aux = aux->getSiguiente();
+            indice++;
+         }
+      }
+
+      if(aux == NULL)
+      {
+         // Nunca encontró el elemento
+         indice = -1;
+      }
+
+      return indice;
+   }
+   
+   public: Tipo busquedaPorIndice(int indice)
+   {
+      Nodo *aux = cabeza;
+      int controlIndice = 0;
+
+      while(controlIndice < indice)
+      {
+         aux = aux->getSiguiente();
+         controlIndice++;
+
+         if(aux = NULL)
+         {
+            // Lanzar error
+         }
+      }
+
+      return aux->getDato();
+   }
+
+   public: void imprimir()
+   {
+      Nodo *aux = cabeza;
+
+      while(aux != NULL)
+      {
+         cout << aux->getDato() << " ";
+         aux = aux->getSiguiente();
+      }
+      cout << endl;
    }
 };
 
@@ -63,6 +103,20 @@ int main()
    lista->insertarAlPrincipio(8);
    lista->insertarAlPrincipio(10);
    lista->insertarAlPrincipio(3);
-   
-   // [5, 10, 8]
+   lista->insertarAlFinal(2);
+   lista->insertarAlPrincipio(3);
+   lista->insertarAlPrincipio(1);
+   lista->insertarAlFinal(9);
+   lista->insertarAlFinal(5);
+   lista->insertarAlPrincipio(0);
+   lista->insertarAlFinal(7);
+   lista->insertarAlPrincipio(1);
+   lista->insertarAlFinal(15);
+
+   lista->imprimir();
+
+   cout << "Lista[4]: " << lista->busquedaPorIndice(4) << endl;
+   cout << "Lista[7]: " << lista->busquedaPorIndice(7) << endl;
+   cout << "Lista[2]: " << lista->busquedaPorIndice(2) << endl;
+   //cout << "Lista[3000]: " << lista->busquedaPorIndice(3000) << endl;
 }
